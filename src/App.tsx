f736869d20e57656021033b514a58d0033e1b75b
ubungmeister <ubungmeister@gmail.com>
2022-09-23 18:50:30 +0200

    dnd bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
 import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 
+import cookie1 from './images/1.png'
+import cookie2 from './images/2.png'
+import cookie3 from './images/3.png'
+import cookie4 from './images/4.png'
+import cookie5 from './images/5.png'
+import cookie6 from './images/6.png'
+
+
 const width = 8
 const cookieName = [
-    'orange',
-    'red',
-    'black',
-    'yellow',
-    'purple',
-    'green'
+    cookie1,
+    cookie2,
+    cookie3,
+    cookie4,
+    cookie5,
+    cookie6
 ]
+console.log(typeof cookieName)
 
 function App() {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([''])
     const [itemBeingDragged, setItemBeingDragged] = useState<Element|null>()
     const [itemBeingReplaced,setItemBeingReplaced] = useState<Element|null>()
-
+    const [score, setScore] = useState(0)
 
     const createBoard = () => {
         const randomCookieArr = []
@@ -34,9 +43,11 @@ function App() {
         for (let i = 0; i <= 39; i++) {
             const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
             const decidedColor = currentColorArrangement[i]
+            if (!validMove()) continue
 
             if (columnOfFour.every(item => currentColorArrangement[item] === decidedColor)) {
                 columnOfFour.forEach(item => currentColorArrangement[item] = '')
+
                 return true
             }
         }
@@ -46,9 +57,10 @@ function App() {
         for (let i = 0; i <= 47; i++) {
             const columnOfThree = [i, i + width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
-
+            if (!validMove()) continue
             if (columnOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
                 columnOfThree.forEach(item => currentColorArrangement[item] = '')
+
                 return true
             }
         }
@@ -59,9 +71,10 @@ function App() {
             const decidedColor = currentColorArrangement[i]
             const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
             if (notValid.includes(i)) continue
-
+            if (!validMove()) continue
             if (rowOffour.every(item => currentColorArrangement[item] === decidedColor)) {
                 rowOffour.forEach(item => currentColorArrangement[item] = '')
+
                 return true
             }
         }
@@ -72,9 +85,10 @@ function App() {
             const decidedColor = currentColorArrangement[i]
             const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
             if (notValid.includes(i)) continue
-
+            if (!validMove()) continue
             if (rowOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
                 rowOfThree.forEach(item => currentColorArrangement[item] = '')
+
                 return true
             }
         }
@@ -95,6 +109,53 @@ function App() {
         }
     }
 
+    const dragStart =(el:Element)=>{
+        setItemBeingDragged(el)
+    }
+    const dragDrop =(el:Element)=>{
+        setItemBeingReplaced(el)
+    }
+    const validMove =()=>{
+        const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
+        const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
+        const validMoves = [
+            itemBeingDraggedID +1,
+            itemBeingDraggedID -1,
+            itemBeingDraggedID - width,
+            itemBeingDraggedID + width
+        ]
+        const isValid = validMoves.includes(itemBeingReplacedId)
+        return isValid
+    }
+
+
+
+    const dragEnd =()=>{
+
+        const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
+        const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
+        const colorBeingDragged = String(itemBeingDragged?.getAttribute('src'))
+        const colorBeingReplaced = String(itemBeingReplaced?.getAttribute('src'))
+
+        currentColorArrangement[itemBeingReplacedId] = colorBeingDragged
+        currentColorArrangement[itemBeingDraggedID] = colorBeingReplaced
+
+
+        const isColumnOfFour =checkColumnOfFour()
+        const isRowOfFour =checkRowOfFour()
+        const isColumnOfThree =checkColumnOfThree()
+        const isRowOfThree = checkRowOfThree()
+
+
+        if(itemBeingReplacedId && (isColumnOfFour||isRowOfFour||isColumnOfThree||isRowOfThree)){
+            setItemBeingDragged(null)
+            setItemBeingReplaced(null)
+        } else {
+            currentColorArrangement[itemBeingReplacedId] = colorBeingReplaced
+            currentColorArrangement[itemBeingDraggedID] = colorBeingDragged
+            setCurrentColorArrangement([...currentColorArrangement])
+        }
+    }
     useEffect(() => {
         const timer = setInterval(() => {
             checkColumnOfFour()
@@ -107,66 +168,23 @@ function App() {
         return () => clearInterval(timer)
     }, [checkColumnOfFour, checkRowOfFour, checkColumnOfThree, checkRowOfThree,currentColorArrangement, moveElementBelow])
 
-    const dragStart =(el:Element)=>{
-        setItemBeingDragged(el)
-    }
-    const dragDrop =(el:Element)=>{
-        setItemBeingReplaced(el)
-    }
-    const dragEnd =(el:Element)=>{
-
-        const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
-        const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
-        const colorBeingDragged = String(itemBeingDragged?.getAttribute('alt'))
-        const colorBeingReplaced = String(itemBeingReplaced?.getAttribute('alt'))
-
-        currentColorArrangement[itemBeingDraggedID] = colorBeingReplaced
-        currentColorArrangement[itemBeingReplacedId] = colorBeingDragged
-
-        const validMoves = [
-            itemBeingDraggedID +1,
-            itemBeingDraggedID -1,
-            itemBeingDraggedID - width,
-            itemBeingDraggedID + width
-        ]
-        const validMove = validMoves.includes(itemBeingReplacedId)
-
-        console.log(validMove)
-
-        const isColumnOfFour =checkColumnOfFour()
-        const isRowOfFour =checkRowOfFour()
-        const isColumnOfThree =checkColumnOfThree()
-        const isRowOfThree = checkRowOfThree()
-
-        if(itemBeingReplacedId && validMove && (isColumnOfFour||isRowOfFour||isColumnOfThree||isRowOfThree)){
-            setItemBeingDragged(null)
-            setItemBeingReplaced(null)
-        } else {
-            currentColorArrangement[itemBeingReplacedId] = colorBeingReplaced
-            currentColorArrangement[itemBeingDraggedID] = colorBeingDragged
-            setCurrentColorArrangement([...currentColorArrangement])
-        }
-    }
-
     return (
         <Container>
+            <div>{score}</div>
             <GameWrapper>
                 {currentColorArrangement.map((el, index) => {
                     return (
                         <IconWrapper
                             key={index}
-                            style={{backgroundColor: el}}
-                            alt = {el}
+                            src={el}
                             data-id={index}
                             draggable={true}
                             onDragStart={(el)=>dragStart(el.currentTarget as Element)}
                             onDragOver={(e)=>e.preventDefault()}
                             onDragEnter={(e)=>e.preventDefault()}
                             onDragLeave={(e)=>e.preventDefault()}
-                            onDrop={(el)=>dragDrop(el.target as Element)}
-                            onDragEnd={(el)=>dragEnd(el.target as Element)}
-
-
+                            onDrop={(el)=>dragDrop(el.currentTarget as Element)}
+                            onDragEnd={dragEnd}
                         />
 
                     )
