b83b5ba2ae5daf2a09e7f67972ab7ce6917afe4e
ubungmeister <ubungmeister@gmail.com>
2022-09-25 14:28:51 +0200

    design update + refactoring in process

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 
-import cookie1 from './images/1.png'
-import cookie2 from './images/2.png'
-import cookie3 from './images/3.png'
-import cookie4 from './images/4.png'
-import cookie5 from './images/5.png'
-import cookie6 from './images/6.png'
+import cookie1 from './images/pastry_cookie01_320.png'
+import cookie2 from './images/pastry_croissant_320@2x.png'
+import cookie3 from './images/pastry_donut_320.png'
+import cookie4 from './images/pastry_macaroon_320.png'
+import cookie5 from './images/pastry_starcookie01_320.png'
+import cookie6 from './images/pastry_cupcake_320.png'
+import backgroundCookie from './images/GameTileBG_01@2x.png'
 
 
 const width = 8
+
 const cookieName = [
     cookie1,
     cookie2,
@@ -18,59 +20,51 @@ const cookieName = [
     cookie5,
     cookie6
 ]
-console.log(typeof cookieName)
 
 function App() {
     const [currentColorArrangement, setCurrentColorArrangement] = useState([''])
-    const [itemBeingDragged, setItemBeingDragged] = useState<Element|null>()
-    const [itemBeingReplaced,setItemBeingReplaced] = useState<Element|null>()
-    const [score, setScore] = useState(0)
+    const [itemBeingDragged, setItemBeingDragged] = useState<Element | null>()
+    const [itemBeingReplaced, setItemBeingReplaced] = useState<Element | null>()
+    const[score, setScore]=useState(0)
 
-    const createBoard = () => {
-        const randomCookieArr = []
-        for (let i = 0; i < width**2; i++) {
-            const randomCookie = cookieName[Math.floor(Math.random() * cookieName.length)]
-            randomCookieArr.push(randomCookie)
-        }
-        setCurrentColorArrangement(randomCookieArr)
-    }
+    //create a new bord 8x8 with random cookies
+    // const createBoard = () => {
+    //     setCurrentColorArrangement(
+    //         [...Array(width * width)].map(
+    //             () => cookieName[Math.floor(Math.random() * cookieName.length)]
+    //         )
+    //     );
+    // } kak prosto peredat znacenie v useState(['']) bez useEffect
+
     useEffect(() => {
-        createBoard()
+        setCurrentColorArrangement(
+            [...Array(width * width)].map(
+                () => cookieName[Math.floor(Math.random() * cookieName.length)]
+            )
+        );
     }, [])
 
 
     const checkColumnOfFour = () => {
-        for (let i = 0; i <= 39; i++) {
+        for (let i = 0; i <= 47; i++) {
             const columnOfFour = [i, i + width, i + width * 2, i + width * 3]
             const decidedColor = currentColorArrangement[i]
-            if (!validMove()) continue
 
+            if (!validMove()) continue
             if (columnOfFour.every(item => currentColorArrangement[item] === decidedColor)) {
                 columnOfFour.forEach(item => currentColorArrangement[item] = '')
-
                 return true
             }
-        }
-    }
 
-    const checkColumnOfThree = () => {
-        for (let i = 0; i <= 47; i++) {
-            const columnOfThree = [i, i + width, i + width * 2]
-            const decidedColor = currentColorArrangement[i]
-            if (!validMove()) continue
-            if (columnOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
-                columnOfThree.forEach(item => currentColorArrangement[item] = '')
-
-                return true
-            }
         }
     }
     const checkRowOfFour = () => {
         for (let i = 0; i < 64; i++) {
             const rowOffour = [i, i + 1, i + 2, i + 3]
             const decidedColor = currentColorArrangement[i]
-            const notValid = [5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53, 54, 55, 62, 63, 64]
-            if (notValid.includes(i)) continue
+            //instead of using indexes of items, we don't need las 3 row to check
+            if (i % 8 >= 5) continue
+
             if (!validMove()) continue
             if (rowOffour.every(item => currentColorArrangement[item] === decidedColor)) {
                 rowOffour.forEach(item => currentColorArrangement[item] = '')
@@ -79,61 +73,69 @@ function App() {
             }
         }
     }
-    const checkRowOfThree = () => {
-        for (let i = 0; i < 64; i++) {
-            const rowOfThree = [i, i + 1, i + 2]
+    const checkColumnOfThree = () => {
+        for (let i = 0; i <= 47; i++) {
+            const columnOfThree = [i, i + width, i + width * 2]
             const decidedColor = currentColorArrangement[i]
-            const notValid = [6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 63, 64]
-            if (notValid.includes(i)) continue
             if (!validMove()) continue
-            if (rowOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
-                rowOfThree.forEach(item => currentColorArrangement[item] = '')
-
+            if (columnOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
+                columnOfThree.forEach(item => currentColorArrangement[item] = '')
                 return true
             }
         }
     }
-    const moveElementBelow =()=>{
-        for(let i=0; i<=55; i++){
-            const firstRow = [0,1,2,3,4,5,6,7]
 
-            if (firstRow.includes(i) && currentColorArrangement[i] === ''){
-                let randomColor = cookieName[Math.floor(Math.random() * cookieName.length)]
-                currentColorArrangement[i]= randomColor
+    const checkRowOfThree = () => {
+        for (let i = 0; i < 64; i++) {
+            const rowOfThree = [i, i + 1, i + 2]
+            const decidedColor = currentColorArrangement[i]
+            //instead of using indexes of items, we don't need las 2 row to check
+            if (i % 8 >= 6) continue
+
+            if (!validMove()) continue
+            if (rowOfThree.every(item => currentColorArrangement[item] === decidedColor)) {
+                rowOfThree.forEach(item => currentColorArrangement[item] = '')
+                return true
+            }
+        }
+    }
+    const moveElementBelow = () => {
+        for (let i = 0; i <= 55; i++) {
+            const firstRow = [0, 1, 2, 3, 4, 5, 6, 7]
+            if (firstRow.includes(i) && currentColorArrangement[i] === '') {
+                currentColorArrangement[i] = cookieName[Math.floor(Math.random() * cookieName.length)]
             }
 
-            if (currentColorArrangement[i+width] === ''){
-                currentColorArrangement[i+width] =currentColorArrangement[i]
-                currentColorArrangement[i]=''
+            if (currentColorArrangement[i + width] === '') {
+                currentColorArrangement[i + width] = currentColorArrangement[i]
+                currentColorArrangement[i] = ''
             }
         }
     }
 
-    const dragStart =(el:Element)=>{
+    const dragStart = (el: Element) => {
         setItemBeingDragged(el)
     }
-    const dragDrop =(el:Element)=>{
+    const dragDrop = (el: Element) => {
         setItemBeingReplaced(el)
     }
-    const validMove =()=>{
-        const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
-        const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
+
+    const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
+    const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
+
+    const validMove = () => {
         const validMoves = [
-            itemBeingDraggedID +1,
-            itemBeingDraggedID -1,
+            itemBeingDraggedID + 1,
+            itemBeingDraggedID - 1,
             itemBeingDraggedID - width,
             itemBeingDraggedID + width
         ]
-        const isValid = validMoves.includes(itemBeingReplacedId)
-        return isValid
+        return validMoves.includes(itemBeingReplacedId)
     }
 
 
+    const dragEnd = () => {
 
-    const dragEnd =()=>{
-
-        const itemBeingDraggedID = Number(itemBeingDragged?.getAttribute('data-id'))
-        const itemBeingReplacedId = Number(itemBeingReplaced?.getAttribute('data-id'))
         const colorBeingDragged = String(itemBeingDragged?.getAttribute('src'))
         const colorBeingReplaced = String(itemBeingReplaced?.getAttribute('src'))
 
@@ -141,36 +143,44 @@ function App() {
         currentColorArrangement[itemBeingDraggedID] = colorBeingReplaced
 
 
-        const isColumnOfFour =checkColumnOfFour()
-        const isRowOfFour =checkRowOfFour()
-        const isColumnOfThree =checkColumnOfThree()
+        const isColumnOfFour = checkColumnOfFour()
+        const isRowOfFour = checkRowOfFour()
+        const isColumnOfThree = checkColumnOfThree()
         const isRowOfThree = checkRowOfThree()
 
 
-        if(itemBeingReplacedId && (isColumnOfFour||isRowOfFour||isColumnOfThree||isRowOfThree)){
+        if (itemBeingReplacedId && (isColumnOfFour || isRowOfFour || isColumnOfThree|| isRowOfThree)) {
             setItemBeingDragged(null)
             setItemBeingReplaced(null)
-        } else {
+
+            // score counter
+            if(isColumnOfFour || isRowOfFour ){
+                setScore((s)=>s+4)
+            }else {setScore((s)=>s+3)}
+        }
+        else {
             currentColorArrangement[itemBeingReplacedId] = colorBeingReplaced
             currentColorArrangement[itemBeingDraggedID] = colorBeingDragged
             setCurrentColorArrangement([...currentColorArrangement])
         }
     }
     useEffect(() => {
+        checkColumnOfFour()
+        checkRowOfFour()
+        checkColumnOfThree()
+        checkRowOfThree()
+        moveElementBelow()
+
         const timer = setInterval(() => {
-            checkColumnOfFour()
-            checkRowOfFour()
-            checkColumnOfThree()
-            checkRowOfThree()
-            moveElementBelow()
             setCurrentColorArrangement([...currentColorArrangement])
         }, 100)
         return () => clearInterval(timer)
-    }, [checkColumnOfFour, checkRowOfFour, checkColumnOfThree, checkRowOfThree,currentColorArrangement, moveElementBelow])
+
+    }, [currentColorArrangement])
 
     return (
         <Container>
-            <div>{score}</div>
+            <ScoreBoardWrapper>Your score: {score}</ScoreBoardWrapper>
             <GameWrapper>
                 {currentColorArrangement.map((el, index) => {
                     return (
@@ -179,11 +189,11 @@ function App() {
                             src={el}
                             data-id={index}
                             draggable={true}
-                            onDragStart={(el)=>dragStart(el.currentTarget as Element)}
-                            onDragOver={(e)=>e.preventDefault()}
-                            onDragEnter={(e)=>e.preventDefault()}
-                            onDragLeave={(e)=>e.preventDefault()}
-                            onDrop={(el)=>dragDrop(el.currentTarget as Element)}
+                            onDragStart={(el) => dragStart(el.currentTarget as Element)}
+                            onDragOver={(e) => e.preventDefault()}
+                            onDragEnter={(e) => e.preventDefault()}
+                            onDragLeave={(e) => e.preventDefault()}
+                            onDrop={(el) => dragDrop(el.currentTarget as Element)}
                             onDragEnd={dragEnd}
                         />
 
@@ -200,15 +210,34 @@ export default App;
 
 const Container = styled.div`
   display: flex;
-  padding: 30px;
+  flex-wrap: wrap;
+  gap: 30px;
+  padding: 60px;
+  border-radius: 10px;
+  
+  
 `
 const GameWrapper = styled.div`
-  width: 560px;
-  height: 560px;
+  width: 480px;
+  height: 480px;
   display: flex;
   flex-wrap: wrap;
+  
 `
 const IconWrapper = styled.img`
-  width: 70px;
-  height: 70px;
+  width: 60px;
+  height: 60px;
+  background-image:url(${backgroundCookie}) ;
+`
+const ScoreBoardWrapper = styled.div`
+  background-image: url(${backgroundCookie});
+  color: white;
+  background-size: 300px;
+  border-radius: 10px;
+  margin-top: 100px;
+  margin-left: 100px;
+  margin-bottom: 350px;
+  min-width: 200px;
+  align-items: center;
+  text-align: center;
 `
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createGlobalStyle} from "styled-components";
 
+import background from './images/darius-anton-coffee-01.gif'
+
+const Global = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    font-family: 'Hanalei Fill', cursive;
+
+  }
+
+  body {
+    background-image: url(${background});
+    background-size: 1300px;
+    min-height: 100vh;
+  }
+  .font-link {
+    font-family: 'Hanalei Fill', cursive;
+  }
+`
 
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
+    <React.StrictMode>
+        <Global/>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
